05440452f9713f0eed6a43ed99fafd2db8276384
Sofia <sofia.montgomery@gmail.com>
2022-01-16 15:44:39 +0100

    added attributes and setters & getters to Squirrel

--- src/main/java/Squirrel.java
+++ src/main/java/Squirrel.java
@@ -1,2 +1,21 @@
 public class Squirrel {
+    int weight;
+    int age;
+    boolean hungry;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
 }
